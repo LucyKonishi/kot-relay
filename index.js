3517e9450e9fb076c34f7a3f9b428a8a94b1486e
LucyKonishi <lucykonishi@gmail.com>
2026-06-05 17:04:30 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cron = require("node-cron");
 const app = express();
 app.use(express.json());
 
+
 const KOT_BASE = "https://api.kingtime.jp/v1.0";
 const KOT_TOKEN = process.env.KOT_TOKEN;
 const KOT_LOGIN_URL = process.env.KOT_LOGIN_URL;
